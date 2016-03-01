0290131d645c47c5bc6c3cf568859a74de8f5bde
Colin Jackson <colin.s.jackson90@gmail.com>
2016-02-29 21:07:03 -0500

    Separate statics into views and assets

--- main.go
+++ main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./staticfiles/"))
-	http.Handle("/staticfiles/", http.StripPrefix("/staticfiles/", fileServer))
+	assetsServer := http.FileServer(http.Dir("./assets/"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", assetsServer))
+
+	pagesServer := http.FileServer(http.Dir("./views/"))
+	http.Handle("/", pagesServer)
+
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
